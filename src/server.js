a15bc5ce5d49b9e74b38d6878c1409b1596278f5
saeid nasiri <name62777@gmail.com>
2020-08-04 12:30:09 +0430

    feat/get ip in nodejs

--- src/server.js
+++ src/server.js
@@ -4,5 +4,8 @@ var date = require('../module/mydatetime')
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   const today = date.myDateTime()
-  res.end(today);
+  var ip = req.connection.remoteAddress
+  console.log(ip)
+  res.write(today);
+  res.end();
 }).listen(8080,()=>{console.log("server run on port localhost:8080")}); 
