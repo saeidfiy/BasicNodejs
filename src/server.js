70de86907bdfa7d972e4a5e17d949bad71b67577
saeid nasiri <name62777@gmail.com>
2020-08-04 12:44:59 +0430

    feat/use html file for home page

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 var date = require('../module/mydatetime')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   const today = date.myDateTime()
   var ip = req.connection.remoteAddress
   console.log(ip)
+  const home = fs.readFileSync('pages/home.html')
+  res.write(home)
   res.write(today);
   res.end();
 }).listen(8080,()=>{console.log("server run on port localhost:8080")}); 
